c7b9533fcd2c5f43e1c64051fddcb1c8a152db5d
Fongoh Martin T <echelon-zeus@Mons-MacBook-Pro.local>
2023-03-23 05:59:53 +0100

    Removed useless imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import LoginForm from './LoginForm';
-import ImageGallery from './ImageGallery';
-import Table from './Table';
+// import LoginForm from './LoginForm';
+// import ImageGallery from './ImageGallery';
+// import Table from './Table';
 import Pans from './Pans';
 
 function App() {
-  const images = [
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/6c3c5d0511999062e185a41afd1a2bea.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/e3ab907d707c10871dd8750a7085d25a.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/cb49c7f833e7a901ca356de7de442f8d.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/0c28f4a113a79ef9a142ff6be0a33502.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/80223ac6f44659625f5c9acbd52a008f.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/d6155484648b3d2b51af5fb9abe9d9c7.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/7a3cbbf08e17f53a590541b4fb600566.JPEG',
-    'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/61b39d3577cc546babc71f05d771c434.JPEG',
-  ];
+  // const images = [
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/6c3c5d0511999062e185a41afd1a2bea.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/e3ab907d707c10871dd8750a7085d25a.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/cb49c7f833e7a901ca356de7de442f8d.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/0c28f4a113a79ef9a142ff6be0a33502.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/80223ac6f44659625f5c9acbd52a008f.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/d6155484648b3d2b51af5fb9abe9d9c7.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/7a3cbbf08e17f53a590541b4fb600566.JPEG',
+  //   'https://digitalrenter.s3.af-south-1.amazonaws.com/listings/61b39d3577cc546babc71f05d771c434.JPEG',
+  // ];
   // <LoginForm onSubmit={(username, password) => console.log('I received', username, password)} />
   // <ImageGallery images={images} />
   return (
